4e1c9865f49f3d91a8a009be43296fc27fa7903f
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2018-10-24 23:27:21 +0900

    aaa

---
+++ vscode-java1/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    int a = 0;
+    a++;
+
+    String s = "ねこ" + a;
+
+    System.out.println(s);
+  }
+}
