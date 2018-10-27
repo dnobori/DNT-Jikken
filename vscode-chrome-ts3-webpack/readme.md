17a10519a5a90dc7c28e12a163fef48d3097aba8
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2018-10-27 14:08:45 +0900

    aaa

---
+++ vscode-chrome-ts3-webpack/readme.md
@@ -0,0 +1,23 @@
+## npm インストール
+```
+npm init -y
+
+npm install --save-dev typescript tslint
+npm install --save-dev webpack webpack-cli ts-loader
+
+node_modules\.bin\tsc --init
+node_modules\.bin\tslint --init
+```
+
+## 設定
+`tsconfig.json` の設定
+
+
+`tslint.json` の設定
+
+
+`vscode/launch.json` の設定
+
+
+`vscode/tasks.json` の設定
+
---
+++ vscode-chrome-ts3-webpack/src/ts/HelloWorld.ts
@@ -0,0 +1,46 @@
+class Startup
+{
+    public static main(): number
+    {
+        let msg = "Hello World " + 31;
+
+        console.log(msg);
+
+        await_test();
+
+        return 0;
+    }
+}
+
+async function await_test()
+{
+    console.log("start");
+
+    for (let i = 0; i < 20; i++)
+    {
+        await test(100);
+        console.log(i);
+    }
+
+    console.log("end");
+}
+
+function test(msec: number)
+{
+    return new Promise(
+        function (resolve, reject)
+        {
+            setTimeout(function ()
+            {
+                //throw "Hello Error";
+                //reject("neko");
+                resolve();
+            },
+                msec);
+        }
+    );
+}
+
+
+Startup.main();
+
