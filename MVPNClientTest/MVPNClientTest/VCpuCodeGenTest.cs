512a16d5b26c9c1ddac73da5dd747de79308b7be
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-02-03 22:42:52 +0900

    Revert "メモリアクセスを関数内関数化 ただし大変遅くなった 失敗"
    This reverts commit eb2c6ed13687ce95455639650311d3ca92a91cb2.

--- MVPNClientTest/MVPNClientTest/VCpuCodeGenTest.cs
+++ MVPNClientTest/MVPNClientTest/VCpuCodeGenTest.cs
@@ -319,12 +319,12 @@ namespace SoftEther.WebSocket.Helper
         {
             StringWriter w = new StringWriter();
             string memcache_tag = null;
+            w.WriteLine("uint write_tmp; uint read_tmp;");
 
             if (this.IsPointer && this.BaseRegister != null && this.Scaler == 0 && this.OffsetRegister == null)
             {
                 memcache_tag = $"memcache_{this.BaseRegister}_0x{this.Displacement:x}";
             }
-            memcache_tag = null;
 
             if (memcache_tag != null)
             {
@@ -338,15 +338,18 @@ namespace SoftEther.WebSocket.Helper
             }
 
 
+            w.WriteLine("{");
 
-            // single page
+            w.WriteLine($"uint vaddr = {GetCode()};");
+            w.WriteLine($"uint vaddr1_index = vaddr / VConsts.PageSize;");
+            w.WriteLine($"uint vaddr1_offset = vaddr % VConsts.PageSize;");
+            //w.WriteLine($"uint vaddr1_offset = vaddr % VConsts.PageSize;");
+            w.WriteLine($"if (vaddr1_index == cache_last_page1)");
             w.WriteLine("{");
             if (writeMode)
             {
-                w.WriteLine($"    write_tmp ={srcOrDestinationCode};");
-
-                w.WriteLine($"    if (mem_write(0x{codeAddress:x}, {GetCode()}, write_tmp) == false) goto L_RETURN;"); 
-
+                w.WriteLine($"     write_tmp = {srcOrDestinationCode};");
+                w.WriteLine($"    *((uint *)(((byte *)cache_last_realaddr1) + vaddr1_offset)) = write_tmp;");
                 if (memcache_tag != null)
                 {
                     w.WriteLine($"    {memcache_tag}_pin = {GetCode()};");
@@ -355,8 +358,7 @@ namespace SoftEther.WebSocket.Helper
             }
             else
             {
-                w.WriteLine($"    if (mem_read(0x{codeAddress:x}, {GetCode()}, &read_tmp) == false) goto L_RETURN;");
-
+                w.WriteLine($"     read_tmp = *((uint *)(((byte *)cache_last_realaddr1) + vaddr1_offset));");
                 w.WriteLine($"    {srcOrDestinationCode}= read_tmp;");
                 if (memcache_tag != null)
                 {
@@ -364,6 +366,111 @@ namespace SoftEther.WebSocket.Helper
                     w.WriteLine($"    {memcache_tag}_data = read_tmp;");
                 }
             }
+            w.WriteLine("}");
+            w.WriteLine("else if (vaddr1_index == cache_last_page2)");
+            w.WriteLine("{");
+            if (writeMode)
+            {
+                w.WriteLine($"     write_tmp = {srcOrDestinationCode};");
+                w.WriteLine($"    *((uint *)(((byte *)cache_last_realaddr2) + vaddr1_offset)) = write_tmp;");
+                if (memcache_tag != null)
+                {
+                    w.WriteLine($"    {memcache_tag}_pin = {GetCode()};");
+                    w.WriteLine($"    {memcache_tag}_data = write_tmp;");
+                }
+            }
+            else
+            {
+                w.WriteLine($"     read_tmp = *((uint *)(((byte *)cache_last_realaddr2) + vaddr1_offset));");
+                w.WriteLine($"    {srcOrDestinationCode}= read_tmp;");
+                if (memcache_tag != null)
+                {
+                    w.WriteLine($"    {memcache_tag}_pin = {GetCode()};");
+                    w.WriteLine($"    {memcache_tag}_data = read_tmp;");
+                }
+            }
+            w.WriteLine("}");
+            w.WriteLine("else");
+            w.WriteLine("{");
+            w.WriteLine($"if (pte[vaddr1_index].{(writeMode ? "CanWrite" : "CanRead")} == false)");
+            w.WriteLine("{");
+            w.WriteLine("    exception_string = $\"Access violation to 0x{vaddr:x}.\";");
+            w.WriteLine($"    exception_address = 0x{codeAddress:x};");
+            w.WriteLine("    goto L_RETURN;");
+            w.WriteLine("}");
+
+            w.WriteLine("if (((last_used_cache++) % 2) == 0)");
+            w.WriteLine("{");
+            w.WriteLine("    cache_last_page1 = vaddr1_index;");
+            w.WriteLine("    cache_last_realaddr1 = pte[vaddr1_index].RealMemory;");
+            w.WriteLine("} else {");
+            w.WriteLine("    cache_last_page2 = vaddr1_index;");
+            w.WriteLine("    cache_last_realaddr2 = pte[vaddr1_index].RealMemory;");
+            w.WriteLine("}");
+            w.WriteLine($"byte *realaddr1 = (byte *)(pte[vaddr1_index].RealMemory + vaddr1_offset);");
+
+            
+            // beyond two pages
+            w.WriteLine("if ((vaddr1_offset + 4) > VConsts.PageSize)");
+            w.WriteLine("{");
+            w.WriteLine("    uint size1 = VConsts.PageSize - vaddr1_offset;");
+            w.WriteLine("    uint size2 = 4 - size1;");
+            w.WriteLine("    uint vaddr2 = vaddr + size1;");
+
+            w.WriteLine($"    uint vaddr2_index = vaddr2 / VConsts.PageSize;");
+            w.WriteLine($"    if (pte[vaddr2_index].{(writeMode ? "CanWrite" : "CanRead")} == false)");
+            w.WriteLine("    {");
+            w.WriteLine("        exception_string = $\"Access violation to 0x{vaddr2:x}.\";");
+            w.WriteLine($"        exception_address = 0x{codeAddress:x};");
+            w.WriteLine("        goto L_RETURN;");
+            w.WriteLine("    }");
+            w.WriteLine($"    byte *realaddr2 = (byte *)(pte[vaddr2_index].RealMemory);");
+            if (writeMode)
+            {
+                w.WriteLine($"    uint set_value = {srcOrDestinationCode};");
+                w.WriteLine("    byte *set_ptr = (byte *)set_value;");
+                w.WriteLine("    if (size1 == 1) { realaddr1[0] = set_ptr[0]; realaddr2[0] = set_ptr[1]; realaddr2[1] = set_ptr[2]; realaddr2[2] = set_ptr[3]; }");
+                w.WriteLine("    else if (size1 == 2) { realaddr1[0] = set_ptr[0]; realaddr1[1] = set_ptr[1]; realaddr2[0] = set_ptr[2]; realaddr2[1] = set_ptr[3]; }");
+                w.WriteLine("    else if (size1 == 3) { realaddr1[0] = set_ptr[0]; realaddr1[1] = set_ptr[1]; realaddr1[2] = set_ptr[2]; realaddr2[0] = set_ptr[3]; }");
+            }
+            else
+            {
+                w.WriteLine($"    uint get_value = 0;");
+                w.WriteLine("    byte *get_ptr = (byte *)get_value;");
+                w.WriteLine("    if (size1 == 1) { get_ptr[0] = realaddr1[0]; get_ptr[1] = realaddr2[0]; get_ptr[2] = realaddr2[1]; get_ptr[3] = realaddr2[2]; }");
+                w.WriteLine("    else if (size1 == 2) { get_ptr[0] = realaddr1[0]; get_ptr[1] = realaddr1[1]; get_ptr[2] = realaddr2[0]; get_ptr[3] = realaddr2[1]; }");
+                w.WriteLine("    else if (size1 == 3) { get_ptr[0] = realaddr1[0]; get_ptr[1] = realaddr1[1]; get_ptr[2] = realaddr1[2]; get_ptr[3] = realaddr2[0]; }");
+                w.WriteLine($"    {srcOrDestinationCode}= get_value;");
+            }
+
+            w.WriteLine("}");
+
+            // single page
+            w.WriteLine("else"); 
+            w.WriteLine("{");
+            if (writeMode)
+            {
+                w.WriteLine($"     write_tmp ={srcOrDestinationCode};");
+                w.WriteLine($"    *((uint *)realaddr1) = write_tmp;");
+                if (memcache_tag != null)
+                {
+                    w.WriteLine($"    {memcache_tag}_pin = {GetCode()};");
+                    w.WriteLine($"    {memcache_tag}_data = write_tmp;");
+                }
+            }
+            else
+            {
+                w.WriteLine($"     read_tmp = *((uint *)realaddr1);");
+                w.WriteLine($"    {srcOrDestinationCode}= read_tmp;");
+                if (memcache_tag != null)
+                {
+                    w.WriteLine($"    {memcache_tag}_pin = {GetCode()};");
+                    w.WriteLine($"    {memcache_tag}_data = read_tmp;");
+                }
+            }
+            w.WriteLine("}");
+            w.WriteLine("}");
+
             w.WriteLine("}");
 
             return w.ToString();
@@ -815,10 +922,6 @@ namespace SoftEther.WebSocket.Helper
             Out.WriteLine("uint cache_last_page2 = 0xffffffff;");
             Out.WriteLine("byte *cache_last_realaddr2 = null;");
 
-            Out.WriteLine("uint write_tmp = 0;");
-            Out.WriteLine("uint read_tmp = 0;");
-
-
             Out.WriteLine("VMemory Memory = state.Memory;");
             Out.WriteLine("VPageTableEntry* pte = Memory.PageTableEntry;");
             Out.WriteLine("uint next_ip = ip;");
@@ -845,135 +948,6 @@ namespace SoftEther.WebSocket.Helper
             }
 
             Out.WriteLine();
-            Out.WriteLine();
-
-            Out.WriteLine(
-@"
-
-// Write
-bool mem_write(uint code_ip, uint vaddr, uint value)
-{
-    uint vaddr1_index = vaddr / VConsts.PageSize;
-    uint vaddr1_offset = vaddr % VConsts.PageSize;
-    if (vaddr1_index == cache_last_page1)
-    {
-        *((uint*)(((byte*)cache_last_realaddr1) + vaddr1_offset)) = value;
-    }
-    else if (vaddr1_index == cache_last_page2)
-    {
-        *((uint*)(((byte*)cache_last_realaddr2) + vaddr1_offset)) = value;
-    }
-    else
-    {
-        if (pte[vaddr1_index].CanWrite == false)
-        {
-            exception_string = $""Access violation to 0x{vaddr:x}."";
-            exception_address = code_ip;
-            return false;
-        }
-        if (((last_used_cache++) % 2) == 0)
-        {
-            cache_last_page1 = vaddr1_index;
-            cache_last_realaddr1 = pte[vaddr1_index].RealMemory;
-        }
-        else
-        {
-            cache_last_page2 = vaddr1_index;
-            cache_last_realaddr2 = pte[vaddr1_index].RealMemory;
-        }
-byte* realaddr1 = (byte*)(pte[vaddr1_index].RealMemory + vaddr1_offset);
-        if ((vaddr1_offset + 4) > VConsts.PageSize)
-        {
-            uint size1 = VConsts.PageSize - vaddr1_offset;
-uint size2 = 4 - size1;
-uint vaddr2 = vaddr + size1;
-uint vaddr2_index = vaddr2 / VConsts.PageSize;
-            if (pte[vaddr2_index].CanWrite == false)
-            {
-                exception_string = $""Access violation to 0x{vaddr2:x}."";
-                exception_address = code_ip;
-                return false;
-            }
-            byte* realaddr2 = (byte*)(pte[vaddr2_index].RealMemory);
-uint set_value = esi;
-byte* set_ptr = (byte*)set_value;
-            if (size1 == 1) { realaddr1[0] = set_ptr[0]; realaddr2[0] = set_ptr[1]; realaddr2[1] = set_ptr[2]; realaddr2[2] = set_ptr[3]; }
-            else if (size1 == 2) { realaddr1[0] = set_ptr[0]; realaddr1[1] = set_ptr[1]; realaddr2[0] = set_ptr[2]; realaddr2[1] = set_ptr[3]; }
-            else if (size1 == 3) { realaddr1[0] = set_ptr[0]; realaddr1[1] = set_ptr[1]; realaddr1[2] = set_ptr[2]; realaddr2[0] = set_ptr[3]; }
-        }
-        else
-        {
-            * ((uint*) realaddr1) = value;
-        }
-    }
-    return true;
-}
-
-bool mem_read(uint code_ip, uint vaddr, uint* value)
-{
-    uint vaddr1_index = vaddr / VConsts.PageSize;
-    uint vaddr1_offset = vaddr % VConsts.PageSize;
-    if (vaddr1_index == cache_last_page1)
-    {
-        *value = *((uint*)(((byte*)cache_last_realaddr1) + vaddr1_offset));
-    }
-    else if (vaddr1_index == cache_last_page2)
-    {
-        *value = *((uint*)(((byte*)cache_last_realaddr2) + vaddr1_offset));
-    }
-    else
-    {
-        if (pte[vaddr1_index].CanRead == false)
-        {
-            exception_string = $""Access violation to 0x{vaddr:x}."";
-            exception_address = code_ip;
-            return false;
-        }
-        if (((last_used_cache++) % 2) == 0)
-        {
-            cache_last_page1 = vaddr1_index;
-            cache_last_realaddr1 = pte[vaddr1_index].RealMemory;
-        }
-        else
-        {
-            cache_last_page2 = vaddr1_index;
-            cache_last_realaddr2 = pte[vaddr1_index].RealMemory;
-        }
-        byte* realaddr1 = (byte*)(pte[vaddr1_index].RealMemory + vaddr1_offset);
-        if ((vaddr1_offset + 4) > VConsts.PageSize)
-        {
-            uint size1 = VConsts.PageSize - vaddr1_offset;
-            uint size2 = 4 - size1;
-            uint vaddr2 = vaddr + size1;
-            uint vaddr2_index = vaddr2 / VConsts.PageSize;
-            if (pte[vaddr2_index].CanRead == false)
-            {
-                exception_string = $""Access violation to 0x{vaddr2:x}."";
-                exception_address = code_ip;
-                return false;
-            }
-            byte* realaddr2 = (byte*)(pte[vaddr2_index].RealMemory);
-            uint get_value = 0;
-            byte* get_ptr = (byte*)get_value;
-            if (size1 == 1) { get_ptr[0] = realaddr1[0]; get_ptr[1] = realaddr2[0]; get_ptr[2] = realaddr2[1]; get_ptr[3] = realaddr2[2]; }
-            else if (size1 == 2) { get_ptr[0] = realaddr1[0]; get_ptr[1] = realaddr1[1]; get_ptr[2] = realaddr2[0]; get_ptr[3] = realaddr2[1]; }
-            else if (size1 == 3) { get_ptr[0] = realaddr1[0]; get_ptr[1] = realaddr1[1]; get_ptr[2] = realaddr1[2]; get_ptr[3] = realaddr2[0]; }
-            *value = get_value;
-        }
-        else
-        {
-            *value = *((uint*)realaddr1);
-        }
-    }
-    return true;
-}
-
-
-");
-
-Out.WriteLine();
-            Out.WriteLine();
-
 
             Out.WriteLine("L_START:");
             Out.WriteLine("switch (next_ip)");
