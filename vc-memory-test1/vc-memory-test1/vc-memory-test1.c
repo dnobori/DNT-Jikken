43b2d85ac034d5a55da503d581de5eaf5feca7c3
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-03-05 18:11:32 +0900

    VirtualAlloc のテスト開始

---
+++ vc-memory-test1/vc-memory-test1/vc-memory-test1.c
@@ -0,0 +1,18 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+
+
+int main()
+{
+    int i;
+    for (i = 0;;i++)
+    {
+        void *p = VirtualAlloc(NULL, 0x100000000ULL, MEM_RESERVE, PAGE_READWRITE);
+        if (p == NULL) Sleep(INFINITE);
+        printf("%p\n", p);
+    }
+    //Sleep(INFINITE);
+    return 0;
+}
+
