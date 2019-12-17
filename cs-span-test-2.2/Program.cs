8c60f70eae2e3cdf56ae676e19421c968f0e4618
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-12-17 13:13:10 +0900

    cs-span-test

---
+++ cs-span-test-2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_span_test_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ cs-span-test-3.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Numerics;
+
+public class Program
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static int Test1(int a, byte b)
+    {
+        Span<byte> tmp1 = new byte[32];
+
+        ref byte r = ref tmp1[a];
+
+        r = b;
+
+        return r;
+    }
+
+    static void Main(string[] args)
+    {
+        Test1(1, 2);
+    }
+}
